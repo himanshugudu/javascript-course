9bc1cb3d4fa5900edadcd6882fc2093fba1b0281
himanshugudu <mangaraj.himansu@gmail.com>
2020-07-25 09:13:22 +0530

    update question

--- question.js
+++ question.js
@@ -20,3 +20,21 @@ let x = function(){
 	}
 }
 console.log(x())
+
+// find no of vowel
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count("Welcome to CodeWOrld"));
